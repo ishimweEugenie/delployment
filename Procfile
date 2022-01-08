2ac36c6a0e1407d5f7d07562b45069f8ab99fa9d
Abou-Shadadi <naslab512@gmail.com>
2022-01-08 12:19:05 +0200

    Updated api version

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: gunicorn Restaurant.wsgi --log-file -
-#web: gunicorn app.wsgi --pythonpath app --log-file -
--- manage.py
+++ manage.py
@@ -6,9 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Assignment_three.settings')
-
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Restaurant.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
