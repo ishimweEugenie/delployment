4d9a3f9ce785630b9efe181f620110e1c24522aa
Abou-Shadadi <naslab512@gmail.com>
2022-01-08 13:13:26 +0200

    Adding token authentications

--- resto/urls.py
+++ resto/urls.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from resto import views
-
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path(r'', views.RestoListAPIView.as_view(), name="Restaurants"),
     path(r'admin/', admin.site.urls),
     path(r'Resto/', views.RestoListAPIView.as_view(), name="Restaurants"),
     path(r'Dishes/', views.DishesListAPIView.as_view(), name="dishes"),
     path(r'District/', views.DishesListAPIView.as_view(), name="districts"),
-    path(r'Sector/', views.DishesListAPIView.as_view(), name="sectors")
+    path(r'Sector/', views.DishesListAPIView.as_view(), name="sectors"),
+
+    path('api-auth/', include('rest_framework.urls')),
+    path('token/', obtain_auth_token, name='api_token_auth'),
 
 ]
