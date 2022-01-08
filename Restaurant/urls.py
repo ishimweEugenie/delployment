3831d22691b1341a92a42325c6c0c97694244f69
Abou-Shadadi <naslab512@gmail.com>
2022-01-08 02:04:26 +0200

    Fix typo error

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Resto.urls')),
+    #path('', include('Resto.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),
